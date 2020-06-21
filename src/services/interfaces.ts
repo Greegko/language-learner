f3fbca24119cf6683255a46b230494cc78cc34d8
Greegko <greegko+dev@gmail.com>
2020-06-21 21:47:35 +0200

    update interface

--- src/services/interfaces.ts
+++ src/services/interfaces.ts
@@ -1,13 +1,5 @@
-export enum Language { English, French };
-
-export interface AutocompleteWord {
-  word: string;
-  language: Language;
-}
-
 export interface Word {
   word: string;
-  meaning: string;
-  sourceLanguage: Language;
-  targetLanguage: Language;
+  translation: string;
+  exampleSentence: string;
 }

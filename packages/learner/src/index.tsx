6a9ad27ca09cea2b1b50ee25dfd676d7ca60319e
Greegko <greegko+dev@gmail.com>
2021-04-14 09:47:10 +0200

    add react router

--- packages/learner/src/index.tsx
+++ packages/learner/src/index.tsx
@@ -1,6 +1,13 @@
 import { App } from './components/app';
 import { render } from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 
 
 const body = document.getElementById('app');
-render(<App />, body);
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  ,
+  body
+);

682e286846bc9da375f5770dcf2b6d8463ca23bc
Greegko <greegko+dev@gmail.com>
2021-12-30 14:08:00 +0100

    add github pages subfolder support

--- packages/learner/src/components/app.tsx
+++ packages/learner/src/components/app.tsx
@@ -19,7 +19,7 @@ export const App = () => {
   }
 
   return (
-    <Routes>
+    <Routes basename={'/language-learner'}>
       <Route path="/deck" element={<DeckPage storage={storage} />}></Route>
       <Route path="/" element={<TrainingPage trainingManager={manager} />}></Route>
     </Routes>
